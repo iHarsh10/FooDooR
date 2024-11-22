cd2bdb48678bb55871c28106461540d43b834f17
Harshvardhan Chandrakar <114882433+iHarsh10@users.noreply.github.com>
2024-11-22 19:58:04 +0000

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -27,13 +27,13 @@ const Footer = () => {
         <div className="footer-content-right">
             <h2>Get In TOUCH</h2>
             <ul>
-                <li>+91-899-899-8998</li>
-                <li>contact@tomato.com</li>
+                <li>6260063552</li>
+                <li>contact@FooDoor.com</li>
             </ul>
         </div>
       </div>
       <hr />
-      <p className="footer-copyright">Copyright 2024 Tomato.com - All Right Reserved.</p>
+      <p className="footer-copyright">Copyright 2024 FooDooR.com - All Right Reserved.</p>
     </div>
   )
 }
